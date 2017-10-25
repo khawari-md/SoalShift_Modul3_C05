ebc7efd9ce4d670bae0be00e3099a950f1087f66
falahrazzaq <32873491+falahrazzaq@users.noreply.github.com>
2017-10-25 10:04:04 +0700

    Create soal 3.c

---
+++ soal 3.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int stats[2];
+void *kepiting () {
+	while (1) {
+
+		sleep(20);
+		stats[0] -= 10;
+		if (stats[0] <= 0 || stats [0] > 100) {
+			exit(EXIT_FAILURE);
+		}
+	}
+}
+void *lohan () {
+	while (1) {
+
+			sleep(20);
+			stats[1] -= 10;
+			if (stats[1] <= 0 || stats [1] > 100) {
+				exit(EXIT_FAILURE);
+			}
+		}
+}
+void makan () {
+	printf("hewan yang di berimakan\n1. kepiting\n2. Lohan\n");
+	int a;
+	scanf ("%d", &a);
+	if (a==1) {
+		stats [0]+=10;
+		printf("Status kepiting bertambah %d\n", stats[0]);
+	}
+	else if (a==2) {
+		stats [1]+=10;
+		printf("Status lohan bertambah %d\n", stats[1]);
+	}
+	
+}
+int main () 	{
+	stats [0]=100;
+	stats [1]=100;
+	pthread_t kepiting_t, lohan_t;
+	pthread_create (&kepiting_t,NULL,&kepiting,NULL);	
+	pthread_create (&lohan_t,NULL,&lohan,NULL);
+
+	while (1) {
+		int pil;
+		if(stats[1] <= 0 || stats[1] > 100 || stats[0] <= 0 || stats[0] > 100) {
+			printf("Status lohan = %d\n", stats[0]);
+			printf("Status kepiting = %d\n", stats[1]);
+			printf("GAME OVER\n");
+			exit(EXIT_FAILURE);
+		}
+		else {
+			printf("1. Lihat status\n2. Berimakan\n");
+			scanf ("%d",&pil);
+
+			if (pil==1) {
+				printf("Status Kepiting: %d\n", stats[0]);
+				printf("Status Lohan: %d\n", stats[1]);
+			}
+			else if (pil==2) {
+				makan();
+			}
+		}
+	}
+}
