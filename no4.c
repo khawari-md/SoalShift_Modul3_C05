fa93acccbbfd8e299c9fc0daa5ac37c3f90b283e
khawari-md <aridzakwan@gmail.com>
2017-10-25 00:00:50 +0000

    nomer 4 kurang input

---
+++ no4.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdlib.h>
+/*******************************************************
+*compile dengan cara gcc -pthread -o [output] input.c *
+*******************************************************/
+
+pthread_t tid[100];
+
+void* faktorial(void *ptr){
+    int i;
+    long long int f;
+    int *N=(int*) ptr;
+    f=1;
+    for(i=2;i<=*N;i++){
+        f=f*i;
+    }
+    printf ("faktorial %d : %lld\n",*N,f);
+    return NULL;
+}
+
+int main(void)
+{
+    
+    int i=0,T,N[]={3,4};
+    int err;
+    T=sizeof(N)/sizeof(N[0]);
+    while(i<T)//looping membuat thread
+    {
+        err=pthread_create(&(tid[i]),NULL,&faktorial,(void *) &N[i]);//membuat thread
+        if(err!=0)//cek error
+        {
+            printf("\n can't create thread : [%s]",strerror(err));
+        }
+        else
+        {
+            //printf("\n create thread success");
+        }
+        i++;
+    }
+    i=0;
+    while(i<T){
+        pthread_join(tid[i],NULL);
+        i++;
+    }
+    return 0;
+}
