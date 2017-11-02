a5c72be579a566218e694e79ceb4029af7faaf33
falahrazzaq <32873491+falahrazzaq@users.noreply.github.com>
2017-11-02 17:04:56 +0700

    Update soal 3.c

--- soal 3.c
+++ soal 3.c
@@ -9,9 +9,15 @@ int stats[2];
 void *kepiting () {
 	while (1) {
 
-		sleep(20);
+		sleep(1);
 		stats[0] -= 10;
-		if (stats[0] <= 0 || stats [0] > 100) {
+		if (stats[0]<=0) {
+		stats [0]=0;
+		}
+		else if (stats[0]>100) {
+		stats [0]=0;
+		}
+		else if (stats[1] == 0 && stats [0] == 0) {
 			exit(EXIT_FAILURE);
 		}
 	}
@@ -19,27 +25,34 @@ void *kepiting () {
 void *lohan () {
 	while (1) {
 
-			sleep(20);
-			stats[1] -= 10;
-			if (stats[1] <= 0 || stats [1] > 100) {
-				exit(EXIT_FAILURE);
-			}
+		sleep(1);
+		stats[1] -= 10;
+		if (stats[1]<=0){
+			stats [1]=0;
 		}
+		else if (stats[1]>100){
+			stats [1]=0;
+		}
+		else if (stats[1] == 0 && stats [0] == 0) {
+				exit(EXIT_FAILURE);
+		}
+	}
 }
 void makan () {
 	printf("hewan yang di berimakan\n1. kepiting\n2. Lohan\n");
 	int a;
 	scanf ("%d", &a);
-	if (a==1) {
-		stats [0]+=10;
-		printf("Status kepiting bertambah %d\n", stats[0]);
-	}
-	else if (a==2) {
-		stats [1]+=10;
-		printf("Status lohan bertambah %d\n", stats[1]);
-	}
-	
+		
+		if (a==1) {
+			stats [0]+=10;
+			printf("Status kepiting bertambah, menjadi %d\n", stats[0]);
+		}
+		else if (a==2) {
+			stats [1]+=10;
+			printf("Status lohan bertambah, menjadi %d\n", stats[1]);
+		}
 }
+	
 int main () 	{
 	stats [0]=100;
 	stats [1]=100;
@@ -49,7 +62,7 @@ int main () 	{
 
 	while (1) {
 		int pil;
-		if(stats[1] <= 0 || stats[1] > 100 || stats[0] <= 0 || stats[0] > 100) {
+		if( stats[1] == 0 && stats [0] == 0 ) {
 			printf("Status kepiting = %d\n", stats[0]);
 			printf("Status lohan = %d\n", stats[1]);
 			printf("GAME OVER\n");
